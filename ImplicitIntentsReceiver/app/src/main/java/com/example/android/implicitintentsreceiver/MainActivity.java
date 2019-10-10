2542e385dae54058b3e5ba6b8fac97f7633654d1
khammami <khammami@gmail.com>
2019-10-10 12:17:19 +0100

    updated gradle version and migrated to androidx

--- ImplicitIntentsReceiver/app/src/main/java/com/example/android/implicitintentsreceiver/MainActivity.java
+++ ImplicitIntentsReceiver/app/src/main/java/com/example/android/implicitintentsreceiver/MainActivity.java
@@ -19,7 +19,7 @@ package com.example.android.implicitintentsreceiver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 /**
