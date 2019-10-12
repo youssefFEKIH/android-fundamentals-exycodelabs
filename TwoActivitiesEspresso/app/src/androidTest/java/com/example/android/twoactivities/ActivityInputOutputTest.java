f1f2ed33ee34a393bc238c377dd621948e4f0489
khammami <khammami@gmail.com>
2019-10-12 14:52:03 +0100

    update gradle & dependencies version and migrated to androidx

--- TwoActivitiesEspresso/app/src/androidTest/java/com/example/android/twoactivities/ActivityInputOutputTest.java
+++ TwoActivitiesEspresso/app/src/androidTest/java/com/example/android/twoactivities/ActivityInputOutputTest.java
@@ -17,21 +17,21 @@
 package com.example.android.twoactivities;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -62,7 +62,7 @@ public class ActivityInputOutputTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.android.twoactivities",
                 appContext.getPackageName());
--- TwoActivitiesEspresso/app/src/main/java/com/example/android/twoactivities/MainActivity.java
+++ TwoActivitiesEspresso/app/src/main/java/com/example/android/twoactivities/MainActivity.java
@@ -18,7 +18,7 @@ package com.example.android.twoactivities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
